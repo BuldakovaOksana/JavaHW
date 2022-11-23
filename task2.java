7d7264c50c2ac74e9514843ac20efa9e48450122
«Buldakova <79263143446@mail.ru>
2022-11-23 15:06:41 +0300

    task3

--- task2.java
+++ task2.java
@@ -1,35 +1,35 @@
+
+/**
+ * task2
+ * Вывести все простые числа от 1 до 1000
+ */
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
 
 public class task2 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the number till which the prime numbers are to be calculated: ");
+        System.out.print("Введите положительное число: ");
         int input = scanner.nextInt();
         scanner.close();
         List<Integer> primes = new ArrayList<>();
 
-        // loop through the numbers one by one (edit include input in range)
         for (int i = 2; i <= input; i++) {
             boolean isPrimeNumber = true;
 
-            // check to see if the number is prime
             for (int j = 2; j < i; j++) {
                 if (i % j == 0) {
                     isPrimeNumber = false;
-                    break; // exit the inner for loop
+                    break;
                 }
             }
 
-            // print the number if prime
             if (isPrimeNumber) {
                 primes.add(i);
             }
         }
-        String s = primes.toString().replace("[", "").replace("]", "");
-        System.out.println("The number of prime is: " + primes.size() + ", and they are: " + s);
+        System.out.println("Простые числа: " + primes);
     }
 }
